6e425a187fd7b0e1b2816362da45c3809292a46f
Florian Clanet <florianclanet@gmail.com>
2021-09-18 08:43:28 +0200

    feat|create home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import { Avatar } from 'primereact/avatar'
+import { Button } from 'primereact/button'
 
 function App() {
+  const data = {
+    name: "Flolight",
+    image: "https://github.com/Flolight.png",
+    bio: "Cloud enthusiast",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/FlolightC",
+        icon: "twitter"
+      }
+    ]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-m-4">
+      <div className="p-d-flex p-jc-center p-ai-center">
+        <Avatar
+          image={data.image}
+          size="xlarge"
+          shape="circle"
+          ></Avatar>
+        <h1 className="p-m-2">{data.name}</h1>
+      </div>
+      <div className="p-d-flex p-jc-center p-ai-center">
+        <p>{data.bio}</p>
+      </div>
+      <div className="p-d-flex p-jc-center p-ai-center">
+        <Button>Change picture</Button>
+      </div>
     </div>
   );
 }
